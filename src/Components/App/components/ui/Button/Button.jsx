61d56e64c5b4d91cdb3af1727e2c7c3bc793bfdc
David Dutas <david.dutas@gmail.com>
2023-05-22 15:59:48 +0200

    Defined propsType

--- src/Components/App/components/ui/Button/Button.jsx
+++ src/Components/App/components/ui/Button/Button.jsx
@@ -1,10 +1,27 @@
-import React, { Children } from 'react'
+import React from 'react'
+import styles from './Button.module.css'
+import PropTypes from 'prop-types'
 
 const Button = (props) => {
     console.log(props)
     return (
-        <button>{ props.children }</button>
+        <button 
+            className={ styles.Button } 
+            onClick={ (evt) => { 
+                props.onButtonClick('hello')
+            }}
+            style={{...props.style, backgroundColor: props.bgColor}}
+        >
+            { props.children }
+        </button>
     );
+}
+
+Button.propTypes={
+    children: PropTypes.any.isRequired,
+    onButtonClick: PropTypes.func.isRequired,
+    bgColor: PropTypes.string,
+    style: PropTypes.object
 }
 
 export default Button;
